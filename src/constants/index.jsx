1e56c100f6519db984b06e729b456484a39d3d2f
Jezreel Fasa <emmanuelfasa@gmail.com>
2024-03-30 09:12:49 +0100

    Changes to files

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -155,26 +155,29 @@ export const footerLinks = [
 ];
 
 export const socialMedia = [
+
+    {
+        id: "social-media-4",
+        icon: linkedin,
+        link: "https://www.linkedin.com/in/emmanuel-odubu-799351168/",
+    },
+
     {
         id: "social-media-1",
         icon: instagram,
-        link: "https://www.instagram.com/",
+        link: "https://www.instagram.com/jezreel_fasa/",
     },
     {
         id: "social-media-2",
         icon: facebook,
-        link: "https://www.facebook.com/",
+        link: "https://web.facebook.com/fasa.jezreel/",
     },
     {
         id: "social-media-3",
         icon: twitter,
-        link: "https://www.twitter.com/",
+        link: "https://twitter.com/Jezreelfasa",
     },
-    {
-        id: "social-media-4",
-        icon: linkedin,
-        link: "https://www.linkedin.com/",
-    },
+    
 ];
 
 export const clients = [
